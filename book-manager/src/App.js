4addf61ec47ca5bfe1d276122bac88daf1e218e9
praakhya <67508200+praakhya@users.noreply.github.com>
2022-10-17 15:24:16 +0530

    Bg

---
+++ book-manager/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import homeBg from './images/homeBg.png';
+import './App.css';
+
+function App()
+{
+  return (
+    <div className="App">
+      <img src={homeBg} width="500px"></img>
+    </div>
+  );
+}
+
+export default App;
