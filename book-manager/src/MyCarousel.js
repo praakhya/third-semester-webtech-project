a129ffe903ad143306f44eb658f154b02a501fd5
praakhya <67508200+praakhya@users.noreply.github.com>
2022-11-08 19:55:56 +0530

    inserted more images

--- book-manager/src/MyCarousel.js
+++ book-manager/src/MyCarousel.js
@@ -9,11 +9,11 @@ function MyCarousel() {
         superLargeDesktop: {
             // the naming can be any, depends on you.
             breakpoint: { max: 4000, min: 3000 },
-            items: 5
+            items: 4
         },
         desktop: {
             breakpoint: { max: 3000, min: 0 },
-            items: 4
+            items: 3
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
@@ -30,8 +30,34 @@ function MyCarousel() {
                 (books) => {
                     var ct = 0;
                     console.log("myCarousel: ",books);
-                    var tagBooks = books.map((b) => { return <div key={ct}><Book name={b.name} cover={b.cover} author={b.auth} /></div> });
-                    return <Carousel responsive={responsive} containerClass="carousel-container" swipeable={true}>
+                    var imagePath="./images/topBooks/";
+                    if (books==null)
+                    {
+                        books=[
+                            {
+                                name:"It Ends With Us",
+                                author: "Colleen Hoover",
+                                cover: `itEndsWithUs.jpg`,
+
+                            },
+                            {
+                                "name":"Go-To Dinners: A Barefoot Contessa Cookbook",
+                                "author":"Ina Garten",
+                                "cover":`goToDinners.jpeg`
+                            },
+                            {
+                                "name":"And There Was Light: Abraham Lincoln and the American Struggle Book",
+                                "author":"Jon Meacham",
+                                "cover":`andThereWasLight.jpg`
+                            }
+                        ]
+                    }
+                    var tagBooks = books.map((b) => { return <div key={ct}><Book name={b.name} cover={imagePath+b.cover} author={b.auth} /></div> });
+                    return <Carousel 
+                    swipeable={true}
+                    responsive={responsive} 
+                    containerClass="carousel-container" 
+                    swipeable={true} arrows={true}>
                         {tagBooks}
                     </Carousel>
                     }
